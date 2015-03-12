5a1ef2cb9c2a430ce3d3876735b6e2694aca41e6
Alec Winograd <awinograd@gmail.com>
2015-03-12 13:16:58 -0700

    Endpoints for kinect + leap SGM demo

--- server.rb
+++ server.rb
@@ -9,4 +9,17 @@ puts "Running OSC server on port #{port}. CTRL-C to stop..."
   puts "#{msg.ip_address}:#{msg.ip_port} -- #{msg.address} -- #{msg.to_a}"
 end
 
+@qc = QuickOsc.new("10.34.165.209", 8090)
+@server.add_method '/hand' do |msg|
+  @qc.send("/tog1/value", msg.to_a.first)
+end
+@server.add_method '/leftradius' do |msg|
+  @qc.send("/sb1/value", msg.to_a.first.to_i)
+end
+@server.add_method '/rightradius' do |msg|
+  @qc.send("/sb2/value", msg.to_a.first.to_i)
+end
+
+@qc.send("/sb1/range", 40, 200)
+@qc.send("/sb2/range", 40, 200)
 @server.run
