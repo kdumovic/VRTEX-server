fe855d8aabbd94240ab27b914e6e87c9ac24c303
Alec Winograd <awinograd@gmail.com>
2015-03-14 15:36:33 -0700

    Add Clients::QuickOscLeapDemo

--- server.rb
+++ server.rb
@@ -22,16 +22,4 @@ end
     clients[full_ip] = client
   end
 end
-@server.add_method '/hand' do |msg|
-  @qc.send("/tog1/value", msg.to_a.first)
-end
-@server.add_method '/leftradius' do |msg|
-  @qc.send("/sb1/value", msg.to_a.first.to_i)
-end
-@server.add_method '/rightradius' do |msg|
-  @qc.send("/sb2/value", msg.to_a.first.to_i)
-end
-
-@qc.send("/sb1/range", 40, 200)
-@qc.send("/sb2/range", 40, 200)
 @server.run
